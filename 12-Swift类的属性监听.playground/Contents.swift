e34d5bda5cf902c6028212de9929f08bec426c85
lei-wen <allenxn@163.com>
2019-04-17 20:41:22 +0800

    Add 12-Swift类的属性监听

---
+++ 12-Swift类的属性监听.playground/Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+class Person: NSObject {
+    // 属性监听器
+    var name : String? {
+        // 属性即将改变时进行监听
+        willSet {
+            print(newValue)
+        }
+        
+        // 属性已经改变时进行监听
+        didSet {
+            print(oldValue)
+        }
+    }
+}
+
+let p = Person()
+p.name = "xx"
+p.name = "yz"
