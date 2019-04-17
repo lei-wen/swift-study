3d56fb0de0c62234d1d85971daaeb52ce254acbc
lei-wen <allenxn@163.com>
2019-04-17 18:58:04 +0800

    Add 07-可选类型用处

---
+++ 07-可选类型用处.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+//类型不一致，不能直接赋值，所以是错的
+//let url : NSURL = NSURL(string: "www.baidu.com")
+
+let url : NSURL? = NSURL(string: "www.baidu.com")
+
+// 根据url创建NSURLRequest对象
+//if url != nil {
+//    let request = NSURLRequest(URL: url!)
+//}
+
+if let url = url {
+    let request = NSURLRequest(url: url as URL)
+    print(request)
+}
